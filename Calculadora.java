e952f9d933df423fabd228fed6f1fbe5bd135cf5
Camilo Tibaduiza <141367348+ingcamilotibaduiza20@users.noreply.github.com>
2023-08-10 15:03:58 -0500

    Add files via upload

--- Calculadora.java
+++ Calculadora.java
@@ -10,18 +10,48 @@ public class Calculadora {
         int res;
         int n1 = 0;
         int n2 = 0;
-        System.out.println("Ingrese la operacion a realizar: \t1. Suma\t2. Resta\t3. Multiplicación\t4. División");
+        System.out.println("Ingrese la operacion a realizar:\n1. Suma\n2. Resta\n3. Multiplicación\n4. División\n5. Potencia\n6. Raiz");
         opc = leer.nextInt();
         
         System.out.println("Ingrese el primer número");
         n1 = leer.nextInt();
         
-        System.out.println("Ingrese el segundo número");
-        n2 = leer.nextInt();
+        if(opc !=6){
+           System.out.println("Ingrese el segundo número");
+        n2 = leer.nextInt(); 
+        }
         
         switch(opc){
-            case 1: res = n1 + n2;
-                System.out.println("La suma de los dos numeros ");
+            case 1 -> {
+                res = n1 + n2;
+                System.out.println("La suma de los dos numeros es: "+res);
+                break;
+            }
+            case 2 -> {
+                res = n1 - n2;
+                System.out.println("La resta de los dos numeros es: "+res);
+                break;
+            }
+            case 3 -> {
+                res = n1 * n2;
+                System.out.println("La multiplicación de los dos numeros es: "+res);
+                break;
+            }
+            case 4 -> {
+                res = n1 / n2;
+                System.out.println("La división entre los dos numeros es: "+res);
+                break;
+            }
+            case 5 -> {
+                res = (int) Math.pow(n1,n2);
+                System.out.println("La potencia entre los dos numeros es: "+res);
+                break;
+            }
+            case 6 -> {
+                res = (int) Math.sqrt(n1);
+                System.out.println("La raíz cuadrada del numero dijitado es: "+res);
+                break;
+            }
         }
     }
 }
