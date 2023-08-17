d58b81787acb2e98e99e8363ed701dae6f9e5361
Camilo Tibaduiza <141367348+ingcamilotibaduiza20@users.noreply.github.com>
2023-08-16 22:16:55 -0500

    Add files via upload

--- Calculadora.java
+++ Calculadora.java
@@ -10,7 +10,7 @@ public class Calculadora {
         int res;
         int n1 = 0;
         int n2 = 0;
-        System.out.println("Ingrese la operacion a realizar:\n1. Suma\n2. Resta\n3. Multiplicación\n4. División\n5. Potencia\n6. Raiz");
+        System.out.println("Ingrese la operacion a realizar:\n1. Suma\n2. Resta\n3. Multiplicación\n4. División\n5. Potencia\n6. Raiz\n7. Seno\n8. Coseno\n9. Tangente\n10. Raiz n\n11. Iva");
         opc = leer.nextInt();
         
         System.out.println("Ingrese el primer número");
@@ -25,32 +25,26 @@ public class Calculadora {
             case 1 -> {
                 res = n1 + n2;
                 System.out.println("La suma de los dos numeros es: "+res);
-                break;
             }
             case 2 -> {
                 res = n1 - n2;
                 System.out.println("La resta de los dos numeros es: "+res);
-                break;
             }
             case 3 -> {
                 res = n1 * n2;
                 System.out.println("La multiplicación de los dos numeros es: "+res);
-                break;
             }
             case 4 -> {
                 res = n1 / n2;
                 System.out.println("La división entre los dos numeros es: "+res);
-                break;
             }
             case 5 -> {
                 res = (int) Math.pow(n1,n2);
                 System.out.println("La potencia entre los dos numeros es: "+res);
-                break;
             }
             case 6 -> {
                 res = (int) Math.sqrt(n1);
                 System.out.println("La raíz cuadrada del numero dijitado es: "+res);
-                break;
             }
             case 7 -> {
                 System.out.println("Ingrese el ángulo en grados:");
@@ -58,7 +52,6 @@ public class Calculadora {
                 double radians = Math.toRadians(angle);
                 double sinValue = Math.sin(radians);
                 System.out.println("El seno de " + angle + " grados es: " + sinValue);
-                break;
             }
             case 8 -> {
                 System.out.println("Ingrese el ángulo en grados:");
@@ -66,7 +59,31 @@ public class Calculadora {
                 double radians = Math.toRadians(angle);
                 double cosValue = Math.cos(radians);
                 System.out.println("El coseno de " + angle + " grados es: " + cosValue);
-                break;
              }
+            case 9 -> {
+                System.out.println("Ingrese el ángulo en grados:");
+                double angle = leer.nextDouble();
+                double radians = Math.toRadians(angle);
+                double tanValue = Math.tan(radians);
+                System.out.println("La tangente de " + angle + " grados es: " + tanValue);
+            }
+            case 10 -> {
+                System.out.println("Ingrese el índice de la raíz:");
+                int index = leer.nextInt();
+                double rootValue = Math.pow(n1, 1.0 / index);
+                System.out.println("La raíz " + index + " de " + n1 + " es: " + rootValue);
+            }
+            case 11 -> {
+                System.out.println("Ingrese el porcentaje de IVA:");
+                double ivaPercentage = leer.nextDouble();
+                System.out.println("Ingrese el valor al que se aplicará el IVA:");
+                double value = leer.nextDouble();
+                double ivaAmount = (ivaPercentage / 100) * value;
+                double totalValue = value + ivaAmount;
+                System.out.println("El monto del IVA es: " + ivaAmount);
+                System.out.println("El total con IVA es: " + totalValue);
+            }
+            default -> System.out.println("Opción no válida.");
+        }
     }
 }
